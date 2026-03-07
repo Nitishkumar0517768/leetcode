1ef1bfe37c7865c629a62a19b8348890105579de
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:02:59 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.c
@@ -0,0 +1,18 @@
+// Last updated: 3/7/2026, 10:03:00 PM
+int removeDuplicates(int* nums, int numsSize) {
+
+    if(numsSize == 0){
+        return 0;
+    }
+
+    int k = 1;
+   
+    for(int i=1; i<numsSize; i++){
+        if(nums[i]!= nums[i-1]){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
