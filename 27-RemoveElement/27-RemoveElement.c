bad8f07aae54e820f11e1627707437ddca4a0f24
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:02:58 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.c
@@ -0,0 +1,13 @@
+// Last updated: 3/7/2026, 10:02:58 PM
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+   
+    for(int i=0; i<numsSize; i++){
+        if(nums[i] != val){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+    return k;
+}
