b7f32551baccf78e102473c1d2a5cfb2b0cea384
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:02:19 +0530

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.c
@@ -0,0 +1,15 @@
+// Last updated: 3/7/2026, 10:02:19 PM
+int countPairs(int* nums, int numsSize, int k) {
+    int count = 0;
+
+    for(int i=0; i<numsSize-1; i++){
+        for(int j=i+1; j<numsSize; j++){
+            if(nums[i] == nums[j]){
+                if((i*j) % k == 0){
+                    count ++;
+                }
+            }
+        }
+    }
+    return count;
+}
