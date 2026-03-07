41bd32c8385c9fbf15e995d6afca46e4a2247d74
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:02:27 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.c
@@ -0,0 +1,17 @@
+// Last updated: 3/7/2026, 10:02:27 PM
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+    int count = 0;
+
+    for(int i=0; i<arrSize; i++){
+        if(arr[i] % 2 ==  1){
+            count ++;
+        }
+        if(arr[i] %2 == 0){
+            count = 0;
+        }
+        if(count == 3){
+            return true;
+        }
+    }
+    return false;
+}
