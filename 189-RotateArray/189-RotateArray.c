b0b2ee4bf8984c1498b1459ca3b6d6200ecb4757
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:02:45 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.c
@@ -0,0 +1,20 @@
+// Last updated: 3/7/2026, 10:02:45 PM
+void rotate(int* nums, int numsSize, int k) {
+    k = k % numsSize;
+    int arr[numsSize];
+    int j=0;
+    for(int i=numsSize-k; i<numsSize; i++){
+        arr[j] = nums[i];
+        j++;
+    }
+    for(int i=0; i<numsSize-k; i++){
+        arr[j] = nums[i];
+        j++;
+    }
+
+    for(int i=0; i<numsSize; i++){
+        nums[i] = arr[i];
+    }
+
+}
+
