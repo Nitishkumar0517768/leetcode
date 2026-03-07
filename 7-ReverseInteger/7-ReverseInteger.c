ed23552bc159b9d355bac3ac995446c27e36bb73
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-07 22:03:02 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.c
@@ -0,0 +1,19 @@
+// Last updated: 3/7/2026, 10:03:02 PM
+int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x = x / 10;
+
+    
+        if (rev > 214748364 || (rev == 214748364 && digit > 7))
+            return 0;
+        if (rev < -214748364 || (rev == -214748364 && digit < -8))
+            return 0;
+
+        rev = rev * 10 + digit;
+    }
+
+    return rev;
+}
