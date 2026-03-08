d74f64ce5b2ce34bf2a8671657f0ab09805deeba
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-08 12:22:41 +0530

    Create

---
+++ problem-1772952760817/problem-1772952760817.c
@@ -0,0 +1,20 @@
+// Last updated: 3/8/2026, 12:22:40 PM
+1#include <limits.h>
+2int minimumIndex(int* capacity, int capacitySize, int itemSize) {
+3    int min = INT_MAX;
+4    int m = 0;
+5    int n = 0;
+6
+7    for(int i=0; i<capacitySize; i++){
+8        if(min > capacity[i] && capacity[i] >= itemSize){
+9            min = capacity[i];
+10            n = i;
+11            m = 1;
+12        }
+13    }
+14
+15    if (m==0){
+16        return -1;
+17    }
+18    return n;
+19}
