ee45d0428469e8a60fa8a2e6d7b2fb3c993cd798
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-12 17:25:25 +0530

    Create

---
+++ problem-1773316525482/problem-1773316525482.js
@@ -0,0 +1,8 @@
+// Last updated: 3/12/2026, 5:25:25 PM
+1/**
+2 * @param {number[]} nums
+3 * @return {void} Do not return anything, modify nums in-place instead.
+4 */
+5var sortColors = function(nums) {
+6    return nums.sort((a,b) => a-b);
+7};
