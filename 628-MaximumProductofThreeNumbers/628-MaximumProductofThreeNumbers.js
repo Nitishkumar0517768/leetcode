9934609e5381cd317e8414e97d6b9cdfc7634fc5
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-03-12 17:17:30 +0530

    File updated at 3/12/2026, 5:17:30 PM

--- 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.js
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.js
@@ -1,26 +1,8 @@
-// Last updated: 3/12/2026, 5:12:07 PM
-1var selfDividingNumbers = function(left, right) {
-2    let arr = [];
-3
-4    for(let i = left; i <= right; i++){
-5        let num = i;
-6        let valid = true;
-7
-8        while(num > 0){
-9            let digit = num % 10;
-10
-11            if(digit === 0 || i % digit !== 0){
-12                valid = false;
-13                break;
-14            }
-15
-16            num = Math.floor(num / 10);
-17        }
-18
-19        if(valid){
-20            arr.push(i);
-21        }
-22    }
-23
-24    return arr;
-25};
+// Last updated: 3/12/2026, 5:17:30 PM
+1/**
+2 * @param {string} s
+3 * @return {string}
+4 */
+5var toLowerCase = function(s) {
+6    return s.toLowerCase();
+7};
