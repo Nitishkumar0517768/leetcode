54987b870b23fdde0b4f2691a6acfd1af91ffec8
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-04-06 10:41:03 +0530

    File updated at 4/6/2026, 10:41:05 AM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -1,22 +1,38 @@
-// Last updated: 4/6/2026, 10:11:28 AM
+// Last updated: 4/6/2026, 10:41:05 AM
 1#include <algorithm>
-2class Solution {
-3public:
-4    int findDuplicate(vector<int>& nums) {
-5        sort(nums.begin(), nums.end());
-6        int n = nums.size();
-7
-8        int i=0;
-9        int j=1;
-10        while(j<n){
-11            if(nums[i] != nums[j]){
-12                i++;
-13                j++;
-14            }
-15            else{
-16                return nums[i];
-17            }
-18        }
-19        return 0;
-20    }
-21};
+2// #include <map>
+3class Solution {
+4public:
+5    int findDuplicate(vector<int>& nums) {
+6
+7        // sort(nums.begin(), nums.end());
+8        // int n = nums.size();
+9
+10        // int i=0;
+11        // int j=1;
+12        // while(j<n){
+13        //     if(nums[i] != nums[j]){
+14        //         i++;
+15        //         j++;
+16        //     }
+17        //     else{
+18        //         return nums[i];
+19        //     }
+20        // }
+21        // return 0;
+22
+23
+24
+25        unordered_map<int, int> freq;
+26
+27        for(int i=0; i<nums.size(); i++){
+28            if(freq[nums[i]] == 0){
+29                freq[nums[i]] = 1;
+30            }
+31            else{
+32                return nums[i];
+33            }
+34        }
+35        return -1;
+36    }
+37};
