922ad0a5b8c510fcd9bee8674315e390a2de9b91
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-04-08 11:57:17 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/8/2026, 11:57:17 AM
+1class Solution {
+2public:
+3    bool isPalindrome(int x) {
+4        if(x<0) return false;
+5
+6        long long int reverse = 0;
+7        int i = x;
+8
+9        while(i > 0){
+10            reverse = reverse * 10 + i%10;
+11            i = i / 10;
+12        }
+13
+14        if(reverse == x){
+15            return true;
+16        }
+17        return false;
+18    }
+19};
