88c6d32212c792826e20d61d23fb60f150cb1b07
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-04-23 14:56:23 +0530

    Create

---
+++ problem-1776936384636/problem-1776936384636.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/23/2026, 2:56:24 PM
+1class Solution {
+2public:
+3    int scoreOfString(string s) {
+4        int sum = 0;
+5
+6        for(int i=0; i<s.size()-1; i++){
+7            int diff = s[i] - s[i+1];
+8
+9            if(diff < 0){
+10                diff = -diff;
+11            }
+12            sum = sum + diff;
+13        }
+14        return sum;
+15    }
+16};
