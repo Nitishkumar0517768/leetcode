29b137ed948f2f5c7ce340c88a7fd9f91cbb0117
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-04-24 11:21:43 +0530

    File updated at 4/24/2026, 11:21:43 AM

--- 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.cpp
+++ 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.cpp
@@ -1,23 +1,28 @@
-// Last updated: 4/24/2026, 11:09:41 AM
+// Last updated: 4/24/2026, 11:21:43 AM
 1class Solution {
 2public:
-3    int numberOfSteps(int num) {
-4        int count = 0;
-5        int n = num;
-6
-7        for(int i=0; i<n; i++){
-8            if(num % 2 == 0){
-9                num = num/2;
-10                count++;
-11            }
-12            if(num % 2 == 1){
-13                num = num - 1;
-14                count++;
-15            }
-16            if(num == 0){
-17                break;
-18            }
-19        }
-20        return count;
-21    }
-22};
+3    bool isPalindrome(string s) {
+4        string res = "";
+5
+6        for(int i=0; i<s.size(); i++){
+7            if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >='0' && s[i] <= '9') ){
+8                s[i] = tolower(s[i]);
+9                res.push_back(s[i]);
+10            }
+11        }
+12
+13        int i=0; 
+14        int j=res.size() - 1;
+15
+16        while(j > i){
+17            if(res[i] == res[j]){
+18                i++;
+19                j--;
+20            }
+21            else{
+22                return false;
+23            }
+24        }
+25        return true;
+26    }
+27};
