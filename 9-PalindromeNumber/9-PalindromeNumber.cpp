74b2e2a1d7dd7fd673d4b21b582067c3f28ee39b
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-02 11:59:10 +0530

    File updated at 5/2/2026, 11:59:16 AM

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -1,18 +1,18 @@
-// Last updated: 4/23/2026, 9:40:40 AM
+// Last updated: 5/2/2026, 11:59:16 AM
 1class Solution {
 2public:
 3    bool isPalindrome(int x) {
-4        if(x<0) return false;
+4        if(x < 0) return false;
 5
 6        long long int reverse = 0;
-7        int i = x;
-8
-9        while(i > 0){
+7
+8        int i = x;
+9        while(i>0){
 10            reverse = reverse * 10 + i%10;
-11            i = i / 10;
+11            i = i/10;
 12        }
 13
-14        if(reverse == x){
+14        if(x == reverse){
 15            return true;
 16        }
 17        return false;
