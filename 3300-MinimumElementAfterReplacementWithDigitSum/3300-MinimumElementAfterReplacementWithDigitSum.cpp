43b29a550105778ffde8a0e0bc887b1c7295bd75
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-05 17:16:26 +0530

    Create 3300-MinimumElementAfterReplacementWithDigitSum

---
+++ 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/5/2026, 5:16:33 PM
+1class Solution {
+2public:
+3    int minElement(vector<int>& nums) {
+4        int min = INT_MAX;
+5        for(int i=0; i<nums.size(); i++){
+6            int digit = 0;
+7            int j=nums[i];
+8            while(j>0){
+9                digit = digit + j%10;
+10                j=j/10;
+11            }
+12            // nums[i] = digit;
+13            if(min > digit){
+14                min = digit;
+15            }
+16        }
+17        return min;
+18
+19    }
+20};
