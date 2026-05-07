0a89cafff559b9ededb04dc7208ed3bf43fe3679
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-07 09:24:36 +0530

    Create 2176-CountEqualandDivisiblePairsinanArray

---
+++ 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 5/7/2026, 9:24:46 AM
+1class Solution {
+2public:
+3    int countPairs(vector<int>& nums, int k) {
+4        int count = 0;
+5
+6        for(int i=0; i<nums.size()-1; i++){
+7            for(int j=i+1; j<nums.size(); j++){
+8                if(nums[i] == nums[j]  && (i*j)%k == 0){
+9                    count++;
+10                }
+11            }
+12        }
+13        return count;
+14    }
+15};
