055d3251bf6e9a9747aa1b44a4c0930f11e87a4c
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 13:04:20 +0530

    File updated at 5/11/2026, 1:04:34 PM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,14 +1,8 @@
-// Last updated: 5/11/2026, 12:58:53 PM
+// Last updated: 5/11/2026, 1:04:34 PM
 1class Solution {
 2public:
 3    int findMin(vector<int>& nums) {
-4        int min = nums[0];
-5
-6        for(int i=0; i<nums.size(); i++){
-7            if(nums[i] < min){
-8                min = nums[i];
-9            }
-10        }
-11        return min;
-12    }
-13};
+4        sort(nums.begin(), nums.end());
+5        return nums[0];
+6    }
+7};
