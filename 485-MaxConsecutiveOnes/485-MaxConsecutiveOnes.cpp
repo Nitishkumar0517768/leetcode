db4c94242a78be3484e64f24e158279260ab0e2e
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 08:57:18 +0530

    File updated at 5/11/2026, 8:57:31 AM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
@@ -1,24 +1,14 @@
-// Last updated: 5/11/2026, 8:46:23 AM
+// Last updated: 5/11/2026, 8:57:31 AM
 1class Solution {
 2public:
-3    int maxPower(string s) {
-4
-5        if(s.size() == 1) return 1;
-6
-7        int count = 1;
-8        int res = 0;
-9
-10        for(int i=0; i<s.size()-1; i++){
-11            if(s[i] == s[i+1]){
-12                count++;
-13            }
-14            else{
-15                count = 1;
-16            }
-17            if(count > res){
-18                res = count;
-19            }
-20        }
-21        return res;
-22    }
-23};
+3    bool checkIfExist(vector<int>& arr) {
+4        for(int i=0; i<arr.size()-1; i++){
+5            for(int j=i+1; j<arr.size(); j++){
+6                if(arr[i] == 2*arr[j] || arr[j] == 2*arr[i]){
+7                    return true;
+8                }
+9            }
+10        }
+11        return false;
+12    }
+13};
