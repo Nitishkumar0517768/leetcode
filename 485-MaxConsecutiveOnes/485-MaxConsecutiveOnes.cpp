9232f252003b3e511c1518e4510785b7fbf6bf29
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 08:38:18 +0530

    Create 485-MaxConsecutiveOnes

---
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/11/2026, 8:38:32 AM
+1class Solution {
+2public:
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int res = nums[0];
+5        int count = 0;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            if(nums[i] == 0){
+9                count = 0;
+10            }
+11            else{ 
+12                count++;
+13            }
+14            if(count > res){
+15                res = count;
+16            }
+17        }
+18        return res;
+19    }
+20};
