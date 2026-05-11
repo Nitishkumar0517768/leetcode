2164f92f6d6361d8984a815465bd1342c23694f1
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 12:13:50 +0530

    File updated at 5/11/2026, 12:14:03 PM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,10 +1,12 @@
-// Last updated: 5/11/2026, 12:06:01 PM
+// Last updated: 5/11/2026, 12:14:03 PM
 1class Solution {
 2public:
 3    bool search(vector<int>& nums, int target) {
 4        for(int i=0; i<nums.size(); i++){
-5            if(nums[i] == target) return true;
-6        }
-7        return false;
-8    }
-9};
+5            if(nums[i] == target){
+6                return true;
+7            }
+8        }
+9        return false;
+10    }
+11};
