d6d030a4787ff16f042d2f794fd53eeef9aade1b
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 11:11:51 +0530

    File updated at 5/11/2026, 11:12:05 AM

--- 27-RemoveElement/27-RemoveElement.cpp
+++ 27-RemoveElement/27-RemoveElement.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/7/2026, 11:42:02 AM
+// Last updated: 5/11/2026, 11:12:05 AM
 1class Solution {
 2public:
 3    int removeElement(vector<int>& nums, int val) {
@@ -7,9 +7,9 @@
 6       for(int i=0; i<nums.size(); i++){
 7            if(nums[i] != val){
 8                nums[count] = nums[i];
-9                count++;
+9                count ++;
 10            }
-11       } 
+11       }
 12       return count;
 13    }
 14};
