fa1d1eeef1a43848a1e54c640c069dbe85120403
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 12:33:02 +0530

    File updated at 5/11/2026, 12:33:16 PM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,12 +1,27 @@
-// Last updated: 5/11/2026, 12:14:03 PM
+// Last updated: 5/11/2026, 12:33:16 PM
 1class Solution {
 2public:
-3    bool search(vector<int>& nums, int target) {
-4        for(int i=0; i<nums.size(); i++){
-5            if(nums[i] == target){
-6                return true;
-7            }
-8        }
-9        return false;
-10    }
-11};
+3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+4        int i = m-1;
+5        int j = n-1;
+6        int k = m+n -1;
+7
+8        while( i>=0 && j>=0){
+9            if(nums1[i] > nums2[j]){
+10                nums1[k] = nums1[i];
+11                i--;
+12            }
+13            else{
+14                nums1[k] = nums2[j];
+15                j--;
+16            }
+17            k--;
+18        }
+19
+20        while (j>=0){
+21            nums1[k] = nums2[j];
+22            j--;
+23            k--;
+24        }
+25    }
+26};
