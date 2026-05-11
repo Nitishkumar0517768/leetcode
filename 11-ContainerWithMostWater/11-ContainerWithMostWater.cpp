5ae89377cb3538fd017319e1f31e253db066a9eb
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 08:25:18 +0530

    File updated at 5/11/2026, 8:25:30 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,30 +1,14 @@
-// Last updated: 5/11/2026, 8:06:17 AM
-#include <algorithm>
-
-class Solution {
-public:
-    bool judgeCircle(string moves) {
-        // If the number of 'U' moves is equal to the number of 'D' moves,
-        // there has no been any vertical movement
-        // Same for 'R' and 'L'.
-        // So simply count the number of occurences of each character and 
-        // check if both those conditions hold
-
-        int x = 0;
-        int y = 0;
-
-        for (auto c : moves) {
-            if (c == 'U') y += 1;
-            else if (c == 'D') y -= 1;
-            else if (c == 'L') x -= 1;
-            else if (c == 'R') x += 1;
-        }
-
-        // int count_U = std::ranges::count(moves, 'U');
-        // int count_D = std::ranges::count(moves, 'D');
-        // int count_L = std::ranges::count(moves, 'L');
-        // int count_R = std::ranges::count(moves, 'R');
-        
-        return x == 0 && y == 0;
-    }
-};
+// Last updated: 5/11/2026, 8:25:30 AM
+1class Solution {
+2public:
+3    int findClosestNumber(vector<int>& nums) {
+4        int res = nums[0];
+5
+6        for(int i=1; i<nums.size(); i++){
+7            if(abs(nums[i]) < abs(res) || (abs(nums[i]) == abs(res) && nums[i] > res)){
+8                res = nums[i];
+9            }
+10        }
+11        return res;
+12    }
+13};
