d4b448a9f2ba120d036e166e552350e4a67e4e7b
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-11 08:06:04 +0530

    File updated at 5/11/2026, 8:06:17 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,15 +1,30 @@
-// Last updated: 5/11/2026, 8:01:41 AM
-1class Solution {
-2public:
-3    bool judgeCircle(string moves) {
-4        unordered_map<char, int> freq;
-5
-6        for(char ch : moves){
-7            freq[ch]++;
-8        }
-9        if(freq['L'] == freq['R']  &&  freq['U'] == freq['D']){
-10            return true;
-11        }
-12        return false;
-13    }
-14};
+// Last updated: 5/11/2026, 8:06:17 AM
+#include <algorithm>
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        // If the number of 'U' moves is equal to the number of 'D' moves,
+        // there has no been any vertical movement
+        // Same for 'R' and 'L'.
+        // So simply count the number of occurences of each character and 
+        // check if both those conditions hold
+
+        int x = 0;
+        int y = 0;
+
+        for (auto c : moves) {
+            if (c == 'U') y += 1;
+            else if (c == 'D') y -= 1;
+            else if (c == 'L') x -= 1;
+            else if (c == 'R') x += 1;
+        }
+
+        // int count_U = std::ranges::count(moves, 'U');
+        // int count_D = std::ranges::count(moves, 'D');
+        // int count_L = std::ranges::count(moves, 'L');
+        // int count_R = std::ranges::count(moves, 'R');
+        
+        return x == 0 && y == 0;
+    }
+};
