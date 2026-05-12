f3891446fd327b01cde7a5c5a14975c53b8f8596
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-12 21:22:51 +0530

    File updated at 5/12/2026, 9:22:51 PM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
@@ -1,11 +1,18 @@
-// Last updated: 5/12/2026, 9:13:54 PM
+// Last updated: 5/12/2026, 9:22:51 PM
 1class Solution {
 2public:
-3    bool checkIfPangram(string sentence) {
-4        set<char> res(sentence.begin(), sentence.end());
-5
-6        if(res.size() < 26) return false;
-7
-8        return true;
-9    }
-10};
+3    string truncateSentence(string s, int k) {
+4        string res = "";
+5        int count = 0;
+6
+7        for(int i=0; i<s.size(); i++){
+8            if(s[i] == ' '){
+9                count++;
+10            }
+11            if(count == k) return res;
+12
+13            res.push_back(s[i]);
+14        }
+15        return res;
+16    }
+17};
