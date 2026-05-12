aec07ae2c5ce637f8eb763aa073f236098e7b411
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-12 21:26:52 +0530

    File updated at 5/12/2026, 9:26:53 PM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
@@ -1,18 +1,18 @@
-// Last updated: 5/12/2026, 9:22:51 PM
-1class Solution {
-2public:
-3    string truncateSentence(string s, int k) {
-4        string res = "";
-5        int count = 0;
-6
-7        for(int i=0; i<s.size(); i++){
-8            if(s[i] == ' '){
-9                count++;
-10            }
-11            if(count == k) return res;
-12
-13            res.push_back(s[i]);
-14        }
-15        return res;
+// Last updated: 5/12/2026, 9:26:53 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    bool checkTree(TreeNode* root) {
+15        return (root->left->val + root->right->val == root->val);
 16    }
 17};
