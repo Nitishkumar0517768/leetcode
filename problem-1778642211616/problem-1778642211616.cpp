4e190c097dc204e5c7fcf5b0c1e62876dc0a83a5
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-13 08:46:51 +0530

    Create

---
+++ problem-1778642211616/problem-1778642211616.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/13/2026, 8:46:51 AM
+1class Solution {
+2public:
+3    int maxProfit(vector<int>& prices) {
+4        int buy = prices[0];
+5        int profit = 0;
+6
+7        for(int i=1; i<prices.size(); i++){
+8            if(buy > prices[i]){
+9                buy = prices[i];
+10            }
+11            else if(prices[i] - buy > profit){
+12                profit = prices[i] - buy;
+13            }
+14        }
+15        return profit;
+16    }
+17};
