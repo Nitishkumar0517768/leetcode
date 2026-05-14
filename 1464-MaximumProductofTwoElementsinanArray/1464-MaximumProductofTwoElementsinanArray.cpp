052b3dd8680a3a51b95844d5107e1d4f3e071190
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-14 08:14:18 +0530

    Create 1464-MaximumProductofTwoElementsinanArray

---
+++ 1464-MaximumProductofTwoElementsinanArray/1464-MaximumProductofTwoElementsinanArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/14/2026, 8:14:19 AM
+1class Solution {
+2public:
+3    int maxProduct(vector<int>& nums) {
+4        int max = INT_MIN;
+5        int secMax = INT_MIN;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            if(max < nums[i]){
+9                secMax = max;
+10                max = nums[i];
+11            }
+12            else if(nums[i] > secMax){
+13                secMax = nums[i];
+14            }
+15        }
+16
+17        
+18
+19        return (max-1) * (secMax-1);
+20    }
+21};
