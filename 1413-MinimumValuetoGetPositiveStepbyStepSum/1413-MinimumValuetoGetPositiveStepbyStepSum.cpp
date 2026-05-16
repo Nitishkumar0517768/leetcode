13a8f4809a4cf94e63cc15452c275dae8b831b5f
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-16 21:29:49 +0530

    File updated at 5/16/2026, 9:29:50 PM

--- 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.cpp
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.cpp
@@ -1,17 +1,14 @@
-// Last updated: 5/16/2026, 9:27:35 PM
+// Last updated: 5/16/2026, 9:29:50 PM
 1class Solution {
 2public:
-3    bool isSameAfterReversals(int num) {
-4        if(num < 10) return true;
+3    int countOdds(int low, int high) {
+4        int count = 0;
 5
-6        int count = 0;
-7
-8        for(int i=num; i>0; i/=10){
-9            int rev = i%10;
-10            if(rev != 0) count++;
-11
-12            if(rev == 0 && count == 0) return false;
-13        }
-14        return true;
-15    }
-16};
+6        for(int i=low; i<=high; i++){
+7            if(i%2 == 1){
+8                count++;
+9            }
+10        }
+11        return count;
+12    }
+13};
