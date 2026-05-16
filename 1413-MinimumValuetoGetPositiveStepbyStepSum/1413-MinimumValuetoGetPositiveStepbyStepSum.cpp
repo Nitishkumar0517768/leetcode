7eacc6909076f0f12be971904fab7cd2138c0374
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-16 21:30:50 +0530

    File updated at 5/16/2026, 9:30:51 PM

--- 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.cpp
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.cpp
@@ -1,14 +1,7 @@
-// Last updated: 5/16/2026, 9:29:50 PM
-1class Solution {
-2public:
-3    int countOdds(int low, int high) {
-4        int count = 0;
-5
-6        for(int i=low; i<=high; i++){
-7            if(i%2 == 1){
-8                count++;
-9            }
-10        }
-11        return count;
-12    }
-13};
+// Last updated: 5/16/2026, 9:30:51 PM
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+};
