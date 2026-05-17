330a8f6b229a06aeeb6eba6456988a84dc542a79
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 09:07:04 +0530

    File updated at 5/17/2026, 9:07:05 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,18 +1,21 @@
-// Last updated: 5/17/2026, 8:51:07 AM
+// Last updated: 5/17/2026, 9:07:05 AM
 1class Solution {
 2public:
-3    int repeatedNTimes(vector<int>& nums) {
+3    int findLucky(vector<int>& arr) {
 4        unordered_map<int, int> freq;
 5
-6        for(int i=0; i<nums.size(); i++){
-7            freq[nums[i]]++;
-8
-9            if(freq[nums[i]] > 1){
-10                return nums[i];
-11            }
-12        }
-13        return -1;
-14
-15        
-16    }
-17};
+6        for (int i = 0; i < arr.size(); i++) {
+7            freq[arr[i]]++;
+8        }
+9
+10        int res = -1;
+11
+12        for (auto& it : freq) {
+13            cout << it.first << " -> " << it.second << endl;
+14            if(it.first == it.second && it.first > res){
+15                res = it.first;
+16            }
+17        }
+18        return res;
+19    }
+20};
