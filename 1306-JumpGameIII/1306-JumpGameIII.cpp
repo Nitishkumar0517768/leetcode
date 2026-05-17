dcd140b0d77c69e47f30964501f56e90db5cf8b4
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 08:10:10 +0530

    File updated at 5/17/2026, 8:10:10 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,21 +1,20 @@
-// Last updated: 5/17/2026, 7:56:11 AM
+// Last updated: 5/17/2026, 8:10:10 AM
 1class Solution {
 2public:
-3    bool check(vector<int>& nums) {
-4        vector<int>check;
-5        for(int x : nums){
-6            check.push_back(x);
-7        }
-8
-9        sort(check.begin(), check.end());
-10
-11        for(int i=0; i<nums.size(); i++){
-12            if(nums == check) return true;
-13
-14            int last = nums.back();
-15            nums.pop_back();
-16            nums.insert(nums.begin(), last);
-17        }
-18        return false;
-19    }
-20};
+3    bool digitCount(string num) {
+4        
+5        for(int i=0; i<num.size(); i++){
+6            int count = 0;
+7            for(int j=0; j<num.size(); j++){
+8                if( i == num[j] - '0'){
+9                    count++;
+10                }
+11            }
+12            // cout << i << "->" << count << endl;
+13            if(count != num[i] - '0'){
+14                return false;
+15            }
+16        }
+17        return true;
+18    }
+19};
