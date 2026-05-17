f37badf9d68f97185247f80d7ee73ae9a033b765
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 08:51:06 +0530

    File updated at 5/17/2026, 8:51:07 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,12 +1,18 @@
-// Last updated: 5/17/2026, 8:39:47 AM
+// Last updated: 5/17/2026, 8:51:07 AM
 1class Solution {
 2public:
-3    int smallestEqual(vector<int>& nums) {
-4        for(int i=0; i<nums.size(); i++){
-5            if(i%10 == nums[i]){
-6                return i;
-7            }
-8        }
-9        return -1;
-10    }
-11};
+3    int repeatedNTimes(vector<int>& nums) {
+4        unordered_map<int, int> freq;
+5
+6        for(int i=0; i<nums.size(); i++){
+7            freq[nums[i]]++;
+8
+9            if(freq[nums[i]] > 1){
+10                return nums[i];
+11            }
+12        }
+13        return -1;
+14
+15        
+16    }
+17};
