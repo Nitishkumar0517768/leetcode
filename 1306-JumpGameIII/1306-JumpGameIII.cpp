d77660c17bde263d3ed8d5add530b9f733adce4f
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 08:31:31 +0530

    File updated at 5/17/2026, 8:31:31 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,18 +1,17 @@
-// Last updated: 5/17/2026, 8:23:35 AM
+// Last updated: 5/17/2026, 8:31:31 AM
 1class Solution {
 2public:
-3    bool checkValid(vector<vector<int>>& matrix) {
-4
-5        for(int i=0; i<matrix.size(); i++){
-6        set<int> row, col;
-7            for(int j=0; j<matrix.size(); j++){
-8                row.insert(matrix[i][j]);
-9                col.insert(matrix[j][i]);
+3    int smallestIndex(vector<int>& nums) {
+4        for(int i=0; i<nums.size(); i++){
+5            int sumDigit = 0;
+6            int n = nums[i];
+7            while(n>0){
+8                sumDigit += n%10;
+9                n /= 10;
 10            }
-11            if(row.size() != matrix.size() || col.size() != matrix.size()){
-12                return false;
-13            }
-14        }
-15        return true;
-16    }
-17};
+11            // cout << sumDigit << endl;
+12            if(sumDigit == i) return i;
+13        }
+14        return -1;
+15    }
+16};
