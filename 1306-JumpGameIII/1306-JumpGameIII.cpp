35c7f12ac5166a782ba90a697a2ee047674b91b1
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 08:39:47 +0530

    File updated at 5/17/2026, 8:39:47 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,17 +1,12 @@
-// Last updated: 5/17/2026, 8:31:31 AM
+// Last updated: 5/17/2026, 8:39:47 AM
 1class Solution {
 2public:
-3    int smallestIndex(vector<int>& nums) {
+3    int smallestEqual(vector<int>& nums) {
 4        for(int i=0; i<nums.size(); i++){
-5            int sumDigit = 0;
-6            int n = nums[i];
-7            while(n>0){
-8                sumDigit += n%10;
-9                n /= 10;
-10            }
-11            // cout << sumDigit << endl;
-12            if(sumDigit == i) return i;
-13        }
-14        return -1;
-15    }
-16};
+5            if(i%10 == nums[i]){
+6                return i;
+7            }
+8        }
+9        return -1;
+10    }
+11};
