e70962c96facf6825c8d9458eaa23e9a5c440020
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-17 08:23:34 +0530

    File updated at 5/17/2026, 8:23:35 AM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,20 +1,18 @@
-// Last updated: 5/17/2026, 8:10:10 AM
+// Last updated: 5/17/2026, 8:23:35 AM
 1class Solution {
 2public:
-3    bool digitCount(string num) {
-4        
-5        for(int i=0; i<num.size(); i++){
-6            int count = 0;
-7            for(int j=0; j<num.size(); j++){
-8                if( i == num[j] - '0'){
-9                    count++;
-10                }
-11            }
-12            // cout << i << "->" << count << endl;
-13            if(count != num[i] - '0'){
-14                return false;
-15            }
-16        }
-17        return true;
-18    }
-19};
+3    bool checkValid(vector<vector<int>>& matrix) {
+4
+5        for(int i=0; i<matrix.size(); i++){
+6        set<int> row, col;
+7            for(int j=0; j<matrix.size(); j++){
+8                row.insert(matrix[i][j]);
+9                col.insert(matrix[j][i]);
+10            }
+11            if(row.size() != matrix.size() || col.size() != matrix.size()){
+12                return false;
+13            }
+14        }
+15        return true;
+16    }
+17};
