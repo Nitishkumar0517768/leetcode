d3440a0144da77673317b1ecc1a6788a94d7f430
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-18 12:50:21 +0530

    File updated at 5/18/2026, 12:50:21 PM

--- 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
+++ 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
@@ -1,12 +1,19 @@
-// Last updated: 5/18/2026, 12:40:54 PM
+// Last updated: 5/18/2026, 12:50:21 PM
 1class Solution {
 2public:
-3    void rotate(vector<int>& nums, int k) {
-4        int n = nums.size();
-5        k = k%n;
+3    vector<int> singleNumber(vector<int>& nums) {
+4        unordered_map<int,int> freq;
+5        vector<int> res;
 6
-7        reverse(nums.begin(), nums.end());
-8        reverse(nums.begin(), nums.begin()+k);
-9        reverse(nums.begin()+k, nums.end());
-10    }
-11};
+7        for(int i=0; i<nums.size(); i++){
+8            freq[nums[i]]++;
+9        }
+10
+11        for(auto it : freq){
+12            if(it.second == 1){
+13                res.push_back(it.first);
+14            }
+15        }
+16        return res;
+17    }
+18};
