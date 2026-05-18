a9ae3610ec84d2a4e8ee8694a1a832517128b07b
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-18 10:32:37 +0530

    Create 260-SingleNumberIII

---
+++ 260-SingleNumberIII/260-SingleNumberIII.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/18/2026, 10:32:37 AM
+1class Solution {
+2public:
+3    vector<int> singleNumber(vector<int>& nums) {
+4        unordered_map<int,int> freq;
+5        vector<int> res;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            freq[nums[i]]++;
+9        }
+10
+11        for(auto it : freq){
+12            if(it.second == 1){
+13                res.push_back(it.first);
+14            }
+15        }
+16        return res;
+17    }
+18};
