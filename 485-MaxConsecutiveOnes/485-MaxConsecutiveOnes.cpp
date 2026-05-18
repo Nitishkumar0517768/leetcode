776873ceeea0bb055aa4d3f2ee79b9131d1c56aa
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-18 10:43:27 +0530

    File updated at 5/18/2026, 10:43:27 AM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.cpp
@@ -1,14 +1,21 @@
-// Last updated: 5/11/2026, 8:57:31 AM
+// Last updated: 5/18/2026, 10:43:27 AM
 1class Solution {
 2public:
-3    bool checkIfExist(vector<int>& arr) {
-4        for(int i=0; i<arr.size()-1; i++){
-5            for(int j=i+1; j<arr.size(); j++){
-6                if(arr[i] == 2*arr[j] || arr[j] == 2*arr[i]){
-7                    return true;
-8                }
-9            }
-10        }
-11        return false;
-12    }
-13};
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int res = 0;
+5        int count = 0;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            if(nums[i] == 0){
+9                count = 0;
+10            }
+11            else{ 
+12                count++;
+13            }
+14            if(count > res){
+15                res = count;
+16            }
+17        }
+18        return res;
+19    }
+20};
