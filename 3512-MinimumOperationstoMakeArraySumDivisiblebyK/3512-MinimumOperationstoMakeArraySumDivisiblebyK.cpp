ec1ceaa7297b968922cdccaabe30a5d930f11b0a
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-18 14:03:38 +0530

    File updated at 5/18/2026, 2:03:38 PM

--- 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
+++ 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
@@ -1,21 +1,25 @@
-// Last updated: 5/18/2026, 1:53:25 PM
+// Last updated: 5/18/2026, 2:03:38 PM
 1class Solution {
 2public:
-3    int findMaxConsecutiveOnes(vector<int>& nums) {
-4        int count  = 0;
-5        int res = 0;
-6
-7        for(int i=0; i<nums.size(); i++){
-8            if(nums[i] == 0){
-9                count = 0;
-10            }
-11            else{
-12                count++;
+3    int dominantIndex(vector<int>& nums) {
+4        int max = INT_MIN;
+5        int secMax = INT_MIN;
+6        int k = 0;
+7
+8        for(int i=0; i<nums.size(); i++){
+9            if(nums[i] > max){
+10                secMax = max;
+11                max = nums[i];
+12                k = i;
 13            }
-14            if(count > res){
-15                res = count;
+14            else if(nums[i] > secMax){
+15                secMax = nums[i];
 16            }
 17        }
-18        return res;
-19    }
-20};
+18
+19        if(secMax*2 <= max){
+20            return k;
+21        }
+22        return -1;
+23    }
+24};
