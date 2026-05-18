39ee193c989cb086d0cf417c33194528c4f073ae
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-18 13:27:21 +0530

    File updated at 5/18/2026, 1:27:21 PM

--- 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
+++ 3512-MinimumOperationstoMakeArraySumDivisiblebyK/3512-MinimumOperationstoMakeArraySumDivisiblebyK.cpp
@@ -1,17 +1,16 @@
-// Last updated: 5/18/2026, 1:13:16 PM
+// Last updated: 5/18/2026, 1:27:21 PM
 1class Solution {
 2public:
 3    int findDuplicate(vector<int>& nums) {
-4
-5        unordered_map<int, int> freq;
-6
-7        for(int i=0; i<nums.size(); i++){
-8            freq[nums[i]]++;
-9
-10            if(freq[nums[i]] > 1){
-11                return nums[i];
-12            }
-13        }
-14        return -1;
-15    }
-16};
+4        unordered_map<int, int> freq;
+5
+6        for(int i=0; i<nums.size(); i++){
+7            freq[nums[i]]++;
+8
+9            if(freq[nums[i]] > 1){
+10                return nums[i];
+11            }
+12        }
+13        return -1;
+14    }
+15};
