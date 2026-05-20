3bc1ba54ab3b0879e184ba4ee48e249eb6f539a9
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-20 09:51:17 +0530

    File updated at 5/20/2026, 9:51:16 AM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -1,11 +1,11 @@
-// Last updated: 4/15/2026, 9:52:05 AM
+// Last updated: 5/20/2026, 9:51:16 AM
 1class Solution {
 2public:
 3    int firstUniqChar(string s) {
-4        unordered_map<char, int>freq;
+4        unordered_map<char, int> freq;
 5
-6        for(char c : s){
-7            freq[c]++;
+6        for(int i=0; i<s.size(); i++){
+7            freq[s[i]]++;
 8        }
 9
 10        for(int i=0; i<s.size(); i++){
