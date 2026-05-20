3d989fd86f198ecfb4aed798cda90b75e1d45acb
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-20 18:15:13 +0530

    File updated at 5/20/2026, 6:15:12 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
@@ -1,11 +1,22 @@
-// Last updated: 5/20/2026, 6:14:31 PM
-1class Solution {
-2public:
-3    vector<int> sortedSquares(vector<int>& nums) {
-4        for(int i=0; i<nums.size(); i++){
-5            nums[i] = nums[i]  * nums[i];
-6        }
-7        sort(nums.begin(), nums.end());
-8        return nums;
-9    }
-10};
+// Last updated: 5/20/2026, 6:15:12 PM
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int st = 0;
+        int en = nums.size()-1;
+        int i = en;
+
+        while(st<=en) {
+            if(abs(nums[st])<abs(nums[en])) {
+                res[i--] = nums[en]*nums[en];
+                en--;
+            }
+            else {
+                res[i--] = nums[st]*nums[st];
+                st++;
+            }
+        }
+        return res;
+    }
+};
