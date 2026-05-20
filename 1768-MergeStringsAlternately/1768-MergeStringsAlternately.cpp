7bb0f6b6a937a4908a3164b577712d59c998714e
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-20 09:37:47 +0530

    File updated at 5/20/2026, 9:37:47 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.cpp
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.cpp
@@ -1,22 +1,26 @@
-// Last updated: 4/24/2026, 10:44:43 AM
+// Last updated: 5/20/2026, 9:37:47 AM
 1class Solution {
 2public:
 3    string mergeAlternately(string word1, string word2) {
 4        string res = "";
-5
-6        int i=0; 
-7        int j=0;
-8
-9        while(i<word1.size() || j<word2.size()){
-10            if(i<word1.size()){
-11                res.push_back(word1[i]);
-12                i++;
-13            }
-14            if(j<word2.size()){
-15                res.push_back(word2[j]);
-16                j++;
-17            }
+5        int i=0; 
+6        int j=0;
+7        
+8        while(i<word1.size() && j<word2.size()){
+9            res = res + word1[i];
+10            res = res + word2[j];
+11            i++;
+12            j++;
+13        }
+14
+15        while(i<word1.size()){
+16            res = res + word1[i];
+17            i++;
 18        }
-19        return res;
-20    }
-21};
+19        while(j<word2.size()){
+20            res = res + word2[j];
+21            j++;
+22        }
+23        return res;
+24    }
+25};
