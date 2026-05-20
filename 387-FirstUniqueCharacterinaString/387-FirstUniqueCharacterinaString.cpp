1e5d19c8303c21b0d3efc8ed751ece3a43389de0
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-20 09:55:21 +0530

    File updated at 5/20/2026, 9:55:21 AM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -1,18 +1,19 @@
-// Last updated: 5/20/2026, 9:51:16 AM
+// Last updated: 5/20/2026, 9:55:21 AM
 1class Solution {
 2public:
-3    int firstUniqChar(string s) {
-4        unordered_map<char, int> freq;
-5
-6        for(int i=0; i<s.size(); i++){
-7            freq[s[i]]++;
-8        }
-9
-10        for(int i=0; i<s.size(); i++){
-11            if(freq[s[i]] == 1){
-12                return i;
+3    bool isBalanced(string num) {
+4        int sum1 = 0;
+5        int sum2 = 0;
+6
+7        for(int i=0; i<num.size(); i++){
+8            if(i%2 == 0){
+9                sum1 = sum1 + (num[i] - '0');
+10            }
+11            else{
+12                sum2 = sum2 + (num[i] - '0');
 13            }
 14        }
-15        return -1;
-16    }
-17};
+15
+16        return sum1 == sum2;
+17    }
+18};
