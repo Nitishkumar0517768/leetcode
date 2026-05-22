31910096c59a20039a06fd5980a60e89f8aa6f3d
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-22 20:57:19 +0530

    File updated at 5/22/2026, 8:57:21 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,14 +1,14 @@
-// Last updated: 4/23/2026, 9:52:33 PM
+// Last updated: 5/22/2026, 8:57:21 PM
 1class Solution {
 2public:
 3    int findMin(vector<int>& nums) {
-4        int n = nums[0];
+4        int min = nums[0];
 5
 6        for(int i=0; i<nums.size(); i++){
-7            if(n > nums[i]){
-8                n = nums[i];
+7            if(nums[i] < min){
+8                min = nums[i];
 9            }
 10        }
-11        return n;
+11        return min;
 12    }
 13};
