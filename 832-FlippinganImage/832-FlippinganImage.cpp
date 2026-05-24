0783e1f9c0c0a9f103b38fab3e9967265ebed587
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-24 13:09:56 +0530

    File updated at 5/24/2026, 1:09:58 PM

--- 832-FlippinganImage/832-FlippinganImage.cpp
+++ 832-FlippinganImage/832-FlippinganImage.cpp
@@ -1,18 +1,12 @@
-// Last updated: 5/24/2026, 12:51:27 PM
+// Last updated: 5/24/2026, 1:09:58 PM
 1class Solution {
 2public:
-3    int maxProfit(vector<int>& prices) {
-4        int buy = prices[0];
-5        int profit = 0;
-6
-7        for(int i=1; i<prices.size(); i++){
-8            if(buy > prices[i]){
-9                buy = prices[i];
-10            }
-11            else if(prices[i]-buy > profit){
-12                profit = prices[i] - buy;
-13            }
-14        }
-15        return profit;
-16    }
-17};
+3    int singleNumber(vector<int>& nums) {
+4        int res = 0;
+5
+6        for(int i=0; i<nums.size(); i++){
+7            res = res ^ nums[i];
+8        }
+9        return res;
+10    }
+11};
