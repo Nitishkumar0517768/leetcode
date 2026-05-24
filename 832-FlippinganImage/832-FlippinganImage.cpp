845fb02a6477fcb87c32a095f2d15dbabe1dc452
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-24 12:12:07 +0530

    File updated at 5/24/2026, 12:12:09 PM

--- 832-FlippinganImage/832-FlippinganImage.cpp
+++ 832-FlippinganImage/832-FlippinganImage.cpp
@@ -1,16 +1,26 @@
-// Last updated: 5/24/2026, 11:56:03 AM
+// Last updated: 5/24/2026, 12:12:09 PM
 1class Solution {
 2public:
-3    int repeatedNTimes(vector<int>& nums) {
-4        unordered_map<int, int> freq;
+3    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+4        vector<vector<int>> res;
 5
-6        for(int i=0; i<nums.size(); i++){
-7            freq[nums[i]]++;
+6        sort(arr.begin(), arr.end());
+7        int diff = INT_MAX;
 8
-9            if(freq[nums[i]] > 1){
-10                return nums[i];
-11            }
-12        }
-13        return -1;
-14    }
-15};
+9        for(int i=0; i<arr.size()-1; i++){
+10            if(abs(arr[i+1] - arr[i]) < diff){
+11                diff = abs(arr[i+1] - arr[i]);
+12            }
+13        }
+14
+15        for(int j=0; j<arr.size()-1; j++){
+16            if(abs(arr[j+1] - arr[j]) == diff){
+17                vector<int> temp;
+18                temp.push_back(arr[j]);
+19                temp.push_back(arr[j+1]);
+20                res.push_back(temp);
+21            }
+22        }
+23        return res;
+24    }
+25};
