6eea6caa1f04866e49f9ca3c788ed6079dc495eb
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-30 12:47:38 +0530

    Create 3289-TheTwoSneakyNumbersofDigitville

---
+++ 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/30/2026, 12:47:39 PM
+1class Solution {
+2public:
+3    vector<int> getSneakyNumbers(vector<int>& nums) {
+4        unordered_map<int, int> freq;
+5        vector<int> res;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            freq[nums[i]]++;
+9        }
+10
+11        for(auto it : freq){
+12            if(it.second == 2){
+13                res.push_back(it.first);
+14            }
+15        }
+16        return res;
+17    }
+18};
