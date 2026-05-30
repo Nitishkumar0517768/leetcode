eb64c5c369ee0a07f0abdd003891f43a882f7ba3
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-30 09:43:14 +0530

    File updated at 5/30/2026, 9:43:14 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,13 +1,15 @@
-// Last updated: 5/30/2026, 9:37:08 AM
+// Last updated: 5/30/2026, 9:43:14 AM
 1class Solution {
 2public:
-3    bool isPowerOfThree(int n) {
-4        if(n <= 0) return false;
+3    bool isPowerOfFour(int n) {
+4        if(n <=0 ) return false;
 5
-6        while(n != 1){
-7            if(n%3 != 0) return false;
-8            n=n/3;
-9        }
-10        return true;
-11    }
-12};
+6        int i=n;
+7        
+8        while(i != 1){
+9            if(i%4 != 0) return false;
+10            i = i/4;
+11        }
+12        return true;
+13    }
+14};
