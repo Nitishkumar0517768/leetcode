ebf6c095b48113e0a8832bdd8332794639f46362
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-30 09:30:40 +0530

    File updated at 5/30/2026, 9:30:40 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,35 +1,17 @@
-// Last updated: 5/30/2026, 9:24:48 AM
+// Last updated: 5/30/2026, 9:30:40 AM
 1class Solution {
 2public:
-3    string reverseWords(string s) {
-4        string temp = "";
-5        vector<string> wordStore;
-6
-7        for(int i=0; i<s.length(); i++){
-8            if(s[i] != ' '){
-9                temp += s[i];
-10            }
-11            else{
-12                if(!temp.empty()){
-13                    wordStore.push_back(temp);
-14                    temp = "";
-15                }
-16            }
-17        }
-18
-19        if(!temp.empty()){
-20            wordStore.push_back(temp);
-21        }
-22
-23        string ans = "";
-24
-25        for(int i=wordStore.size()-1; i>=0; i--){
-26            ans += wordStore[i];
-27
-28            if(i!=0){
-29                ans += " ";
-30            }
-31        }
-32        return ans;
-33    }
-34};
+3    bool isPowerOfTwo(int n) {
+4       if(n <= 0) return false;
+5
+6       int i = n;
+7
+8       while(i!=1){
+9            if(i%2 == 1){
+10                return false;
+11            }
+12            i = i/2;
+13       }
+14       return true;
+15    }
+16};
