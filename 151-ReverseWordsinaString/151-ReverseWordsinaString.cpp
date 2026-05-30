3a2082b75b9ebc93cb8c368f082a84bcb48e3a48
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-30 09:24:48 +0530

    File updated at 5/30/2026, 9:24:48 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,33 +1,35 @@
-// Last updated: 4/11/2026, 3:02:07 PM
+// Last updated: 5/30/2026, 9:24:48 AM
 1class Solution {
 2public:
 3    string reverseWords(string s) {
 4        string temp = "";
 5        vector<string> wordStore;
 6
-7        for (int i = 0; i < s.length(); i++) {
-8            if (s[i] != ' ') {
+7        for(int i=0; i<s.length(); i++){
+8            if(s[i] != ' '){
 9                temp += s[i];
-10            } else {
-11                if (!temp.empty()) {
-12                    wordStore.push_back(temp);
-13                    temp = "";
-14                }
-15            }
-16        }
-17
-18        
-19        if (!temp.empty()) {
+10            }
+11            else{
+12                if(!temp.empty()){
+13                    wordStore.push_back(temp);
+14                    temp = "";
+15                }
+16            }
+17        }
+18
+19        if(!temp.empty()){
 20            wordStore.push_back(temp);
 21        }
 22
-23      
-24        string ans = "";
-25        for (int i = wordStore.size() - 1; i >= 0; i--) {
+23        string ans = "";
+24
+25        for(int i=wordStore.size()-1; i>=0; i--){
 26            ans += wordStore[i];
-27            if (i != 0) ans += " ";
-28        }
-29
-30        return ans;
-31    }
-32};
+27
+28            if(i!=0){
+29                ans += " ";
+30            }
+31        }
+32        return ans;
+33    }
+34};
