8655c5fbf73a7eaf7668f1af291a33593762a9b4
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-05-30 09:36:14 +0530

    File updated at 5/30/2026, 9:36:14 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cpp
@@ -1,17 +1,13 @@
-// Last updated: 5/30/2026, 9:30:40 AM
+// Last updated: 5/30/2026, 9:36:14 AM
 1class Solution {
 2public:
-3    bool isPowerOfTwo(int n) {
-4       if(n <= 0) return false;
+3    bool isPowerOfThree(int n) {
+4        if(n <= 0) return false;
 5
-6       int i = n;
-7
-8       while(i!=1){
-9            if(i%2 == 1){
-10                return false;
-11            }
-12            i = i/2;
-13       }
-14       return true;
-15    }
-16};
+6        while(n != 1){
+7            if(n%3 != 0) return false;
+8            n=n/3;
+9        }
+10        return true;
+11    }
+12};
