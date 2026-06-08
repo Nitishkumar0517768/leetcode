1eccfd18d7d1cfab7314a6946fe2221dbfec6646
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-06-08 11:55:44 +0530

    File updated at 6/8/2026, 11:55:45 AM

--- 228-SummaryRanges/228-SummaryRanges.cpp
+++ 228-SummaryRanges/228-SummaryRanges.cpp
@@ -1,25 +1,16 @@
-// Last updated: 6/8/2026, 11:35:15 AM
+// Last updated: 6/8/2026, 11:55:45 AM
 1class Solution {
 2public:
-3    vector<string> summaryRanges(vector<int>& nums) {
-4        vector<string> res;
-5        
-6        for(int i=0; i<nums.size(); i++){
-7            int start = nums[i];
-8
-9            while(i+1 < nums.size() && nums[i+1]==nums[i]+1){
-10                i++;
-11            }
+3    bool containsDuplicate(vector<int>& nums) {
+4        unordered_set<int> st;
+5
+6        for(int num : nums){
+7            if(st.find(num) != st.end()){
+8                return true;
+9            }
+10            st.insert(num);
+11        }
 12
-13            int end = nums[i];
-14
-15             if(start == end) {
-16                res.push_back(to_string(start));
-17            } else {
-18                res.push_back(to_string(start) + "->" + to_string(end));
-19            }
-20        }
-21
-22        return res;
-23    }
-24};
+13        return false;
+14    }
+15};
