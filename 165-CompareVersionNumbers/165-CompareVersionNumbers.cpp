e6eea82f2c3d9aa14a5ef1bda0a5f637ae1422b5
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-06-16 15:27:39 +0530

    File updated at 6/16/2026, 3:27:39 PM

--- 165-CompareVersionNumbers/165-CompareVersionNumbers.cpp
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.cpp
@@ -1,43 +1,37 @@
-// Last updated: 6/16/2026, 3:13:37 PM
+// Last updated: 6/16/2026, 3:27:39 PM
 1class Solution {
 2public:
 3    int compareVersion(string version1, string version2) {
-4
-5        while (!version1.empty() || !version2.empty()) {
+4        int n = version1.size();
+5        int m = version2.size();
 6
-7            string v1 = "", v2 = "";
+7        int i = 0, j = 0;
 8
-9            int i = 0;
-10            while (i < version1.size() && version1[i] != '.') {
-11                v1 += version1[i];
-12                i++;
-13            }
-14
-15            if (i < version1.size()) { // '.' mila
-16                version1.erase(0, i + 1);
-17            } else {
-18                version1.erase(0, i);
+9        while (i < n || j < m) {
+10            int num1 = 0, num2 = 0;
+11
+12            while (i < n) {
+13                if (version1[i] == '.') {
+14                    i++;
+15                    break;
+16                }
+17                num1 = num1 * 10 + (version1[i] - '0');
+18                i++;
 19            }
 20
-21            int j = 0;
-22            while (j < version2.size() && version2[j] != '.') {
-23                v2 += version2[j];
-24                j++;
-25            }
-26
-27            if (j < version2.size()) { // '.' mila
-28                version2.erase(0, j + 1);
-29            } else {
-30                version2.erase(0, j);
-31            }
-32
-33            int num1 = v1.empty() ? 0 : stoi(v1);
-34            int num2 = v2.empty() ? 0 : stoi(v2);
-35
-36            if (num1 > num2) return 1;
-37            if (num1 < num2) return -1;
-38        }
-39
-40        return 0;
-41    }
-42};
+21            while (j < m) {
+22                if (version2[j] == '.') {
+23                    j++;
+24                    break;
+25                }
+26                num2 = num2 * 10 + (version2[j] - '0');
+27                j++;
+28            }
+29
+30            if (num1 < num2) return -1;
+31            if (num1 > num2) return 1;
+32        }
+33
+34        return 0;
+35    }
+36};
