9c9ba9bd7b5b7ab37a22ed403d965f0224a61fff
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-07-06 13:20:32 +0530

    Create 1588-SumofAllOddLengthSubarrays

---
+++ 1588-SumofAllOddLengthSubarrays/1588-SumofAllOddLengthSubarrays.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/6/2026, 1:20:32 PM
+1class Solution {
+2public:
+3    int sumOddLengthSubarrays(vector<int>& arr) {
+4        int n = arr.size();
+5        int ans = 0;
+6
+7        for (int len = 1; len <= n; len += 2) {
+8            for (int i = 0; i + len <= n; i++) {
+9                int sum = 0;
+10                for (int j = i; j < i + len; j++) {
+11                    sum += arr[j];
+12                }
+13                ans += sum;
+14            }
+15        }
+16
+17        return ans;
+18    }
+19};
