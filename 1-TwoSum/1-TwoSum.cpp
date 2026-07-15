e0aa23a2a0d360aa882f2b4858457412624d1316
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-07-15 22:43:30 +0530

    File updated at 7/15/2026, 10:43:32 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,18 +1,19 @@
-// Last updated: 4/23/2026, 9:13:38 AM
+// Last updated: 7/15/2026, 10:43:32 PM
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
-4        vector<int> v;
+4        map<int,int> mpp;
 5
-6        for(int i=0; i<nums.size()-1; i++){
-7            for(int j=i+1; j<nums.size(); j++){
-8                if(nums[i] + nums[j] == target){
-9                    v.push_back(i);
-10                    v.push_back(j);
-11                    return v;
-12                }
-13            }
-14        }
-15        return v;
-16    }
-17};
+6        for(int i=0; i<nums.size(); i++){
+7            int num = nums[i];
+8            int req = target - num;
+9
+10            if(mpp.find(req) != mpp.end()){
+11                return {mpp[req], i};
+12            }
+13
+14            mpp[num] = i;
+15        }
+16        return {-1, -1};
+17    }
+18};
