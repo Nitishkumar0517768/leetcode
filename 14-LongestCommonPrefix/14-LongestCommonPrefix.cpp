90bc004d9d8d608616442569a327cc707c8c0c43
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-07-15 23:00:39 +0530

    File updated at 7/15/2026, 11:00:40 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,24 +1,17 @@
-// Last updated: 4/23/2026, 9:41:01 AM
+// Last updated: 7/15/2026, 11:00:40 PM
 1class Solution {
 2public:
 3    string longestCommonPrefix(vector<string>& strs) {
 4        string res = "";
-5        int count = 0;
-6
-7        for(int i=0; i<strs[0].size(); i++){
-8                count = 0;
-9            for(int j=0; j<strs.size()-1; j++){
-10                if(strs[j][i] == strs[j+1][i]){
-11                    count++;
-12                }
-13            }
-14            if(count == strs.size()-1){
-15                res.push_back(strs[0][i]);
-16            }
-17            else{
-18                return res;
-19            }
-20        }
-21        return res;
-22    }
-23};
+5
+6        for(int i=0; i<strs[0].size(); i++){
+7            for(int j=0; j<strs.size()-1; j++){
+8                if(strs[j][i] != strs[j+1][i]){
+9                    return res;
+10                }
+11            }
+12            res += strs[0][i];
+13        }
+14        return res;
+15    }
+16};
