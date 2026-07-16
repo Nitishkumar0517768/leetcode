3dc07950f6296445b9d3a1acfa49f1906c43aa4c
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-07-16 21:19:19 +0530

    File updated at 7/16/2026, 9:19:19 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,17 +1,17 @@
-// Last updated: 4/7/2026, 11:19:07 AM
+// Last updated: 7/16/2026, 9:19:19 PM
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        unordered_map <int, int> freq;
-5
-6        for(int i=0; i<nums.size(); i++){
-7        
-8                freq[nums[i]]++;
+4        unordered_map<int, int> freq;
+5        int n = nums.size();
+6
+7        for(int i=0; i<n; i++){
+8            freq[nums[i]]++;
 9            
-10            if(freq[nums[i]] > nums.size()/2){
+10            if(freq[nums[i]] > n/2){
 11                return nums[i];
 12            }
 13        }
-14        return 0;
+14        return -1;
 15    }
 16};
