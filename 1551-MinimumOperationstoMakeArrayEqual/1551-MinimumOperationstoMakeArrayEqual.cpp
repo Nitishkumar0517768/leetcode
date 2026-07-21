cbdf66113e432f28c76136f15422b9c0edb63d24
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-07-21 17:51:26 +0530

    Create 1551-MinimumOperationstoMakeArrayEqual

---
+++ 1551-MinimumOperationstoMakeArrayEqual/1551-MinimumOperationstoMakeArrayEqual.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/21/2026, 5:51:26 PM
+1class Solution {
+2public:
+3    int minOperations(int n) {
+4        int i;
+5        int ans = 0;
+6        if(n%2 == 0){
+7            i = 1;
+8        }
+9        else{
+10            i = 2;
+11        }
+12
+13        while(i<n){
+14            ans += i;
+15            i += 2;
+16        }
+17        return ans;
+18    }
+19};
