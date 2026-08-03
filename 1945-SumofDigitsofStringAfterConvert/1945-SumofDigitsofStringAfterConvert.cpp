3e8bd706e3e1c368c9c0f38336968d4858eff9c6
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-03 17:58:37 +0530

    File updated at 8/3/2026, 5:58:38 PM

--- 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
+++ 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
@@ -1,25 +1,36 @@
-// Last updated: 8/2/2026, 9:50:27 PM
+// Last updated: 8/3/2026, 5:58:38 PM
 1class Solution {
 2public:
-3    int getLucky(string s, int k) {
-4        string m = "";
-5        string temp1;
+3    string digitSum(string s, int k) {
+4        string temp1 = s;
+5        string temp2 = "";
 6
-7        for(int i=0; i<s.size(); i++){
-8            int temp = s[i] - 'a' + 1;
-9            m += to_string(temp);
-10        }
-11
-12        temp1 = m;
-13
-14        for(int j=0; j<k; j++){
-15            int sum = 0;
-16            for(int l=0; l<temp1.size(); l++){
-17                sum += temp1[l] - '0';
-18            }
-19            temp1 = to_string(sum);
-20        }
-21
-22        return stoi(temp1);
-23    }
-24};
+7        while (true) {
+8            int sum = 0;
+9            int count = 0;
+10
+11            if (temp1.size() <= k) {
+12                return temp1;
+13            }
+14            for (int i = 0; i < temp1.size(); i++) {
+15                sum += temp1[i] - '0';
+16                count++;
+17
+18                if (count == k) {
+19                    temp2 += to_string(sum);
+20                    sum = 0;
+21                    count = 0;
+22                }
+23
+24 
+25                if (i == temp1.size() - 1 && count > 0) {
+26                    temp2 += to_string(sum);
+27                }
+28            }
+29
+30
+31            temp1 = temp2;
+32            temp2 = "";
+33        }
+34    }
+35};
