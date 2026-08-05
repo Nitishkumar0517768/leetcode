acdcb2704546b0c93b9af1157e1b73d6ab3e9062
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-05 15:45:48 +0530

    File updated at 8/5/2026, 3:45:47 PM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.cpp
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/5/2026, 3:37:39 PM
+// Last updated: 8/5/2026, 3:45:47 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -10,44 +10,56 @@
 9class Solution {
 10public:
 11    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-12        ListNode* curr = headA;
-13        int countA = 0;
-14        int countB = 0;
-15
-16        while(curr != nullptr){
-17            countA++;
-18            curr = curr->next;
-19        }
-20
-21        curr = headB;
-22        while(curr != nullptr){
-23            countB++;
-24            curr = curr->next;
-25        }
-26
-27        if(countA > countB){
-28            countA = countA-countB;
-29            int count=0;
-30            while(count < countA){
-31                headA = headA->next;
-32                count++;
-33            }
-34        }
-35        else{
-36            int count = 0;
-37            countB = countB - countA;
-38            while(count < countB){
-39                headB = headB->next;
-40                count++;
-41            }
-42        }
-43
-44        while(headA != headB){
-45            headA = headA->next;
-46            headB = headB->next;
-47        }
-48        return headA;
-49        
-50        
-51    }
-52};
+12        
+13        
+14        // ListNode* curr = headA;
+15        // int countA = 0;
+16        // int countB = 0;
+17
+18        // while(curr != nullptr){
+19        //     countA++;
+20        //     curr = curr->next;
+21        // }
+22
+23        // curr = headB;
+24        // while(curr != nullptr){
+25        //     countB++;
+26        //     curr = curr->next;
+27        // }
+28
+29        // if(countA > countB){
+30        //     countA = countA-countB;
+31        //     int count=0;
+32        //     while(count < countA){
+33        //         headA = headA->next;
+34        //         count++;
+35        //     }
+36        // }
+37        // else{
+38        //     int count = 0;
+39        //     countB = countB - countA;
+40        //     while(count < countB){
+41        //         headB = headB->next;
+42        //         count++;
+43        //     }
+44        // }
+45
+46        // while(headA != headB){
+47        //     headA = headA->next;
+48        //     headB = headB->next;
+49        // }
+50        // return headA;
+51        
+52        
+53
+54        // Method-B
+55        ListNode* a = headA;
+56        ListNode* b = headB;
+57
+58        while(a != b){
+59            a = (a==NULL?headA:a->next);
+60            b = (b==NULL?headB : b->next);
+61        }
+62        return a;
+63    }
+64};
