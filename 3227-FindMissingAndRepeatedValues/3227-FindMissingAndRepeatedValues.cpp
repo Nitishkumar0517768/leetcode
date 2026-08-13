01310a262d88bbd985376283d0a9d216a7ad55a5
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-13 10:38:03 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/13/2026, 10:38:03 AM
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<int> freq(n*n + 1, 0);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                freq[grid[i][j]]++;
+            }
+        }
+
+        int rep = -1; 
+        int miss = -1;
+
+        for(int i=1; i<=n*n; i++){
+            if(freq[i] == 2){
+                rep = i;
+            }
+            if(freq[i] == 0){
+                miss = i;
+            }
+        }
+
+        return {rep, miss};
+    }
+};
