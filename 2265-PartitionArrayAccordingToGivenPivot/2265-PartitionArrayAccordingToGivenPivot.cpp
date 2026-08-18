136e2073d8cff4567cfcafb648050a83b14a85d9
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:48:02 +0530

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/18/2026, 11:48:03 AM
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> arr;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] < pivot){
+                arr.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == pivot){
+                arr.push_back(nums[i]);
+            }
+        }
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] > pivot){
+                arr.push_back(nums[i]);
+            }
+        }
+        return arr;
+    }
+};
