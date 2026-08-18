bb71fd482f79ae606334d37ec8ac428fea46d9fd
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:51:30 +0530

    Create 1072-NextGreaterNodeInLinkedList

---
+++ 1072-NextGreaterNodeInLinkedList/1072-NextGreaterNodeInLinkedList.cpp
@@ -0,0 +1,39 @@
+// Last updated: 8/18/2026, 11:51:31 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> ans;
+
+        while(head != nullptr){
+            ans.push_back(head->val);
+            head = head->next;
+        }
+
+        for(int i=0; i<ans.size(); i++){
+            int count = 0;
+            for(int j=i+1; j<ans.size(); j++){
+                if(ans[i] < ans[j]){
+                    ans[i] = ans[j];
+                    count = 1;
+                    break;
+                }
+            }
+            if(count == 0){
+                ans[i] = 0;
+            }
+        }
+
+
+        return ans;
+    }
+};
