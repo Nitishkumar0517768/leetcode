ee921819e4b08dc7ab2ec84828ddc49abdeccf2d
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:51:39 +0530

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 8/18/2026, 11:51:40 AM
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            nums[i] = nums[i]  * nums[i];
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
