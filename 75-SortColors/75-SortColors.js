fd58520d198c579c58358a64c9c02a29bfc1c782
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:54:50 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.js
@@ -0,0 +1,8 @@
+// Last updated: 8/18/2026, 11:54:51 AM
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    return nums.sort((a,b) => a-b);
+};
