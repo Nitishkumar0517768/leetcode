c70029b5212abc3beb20ad52b5fd60654b07903d
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:48:29 +0530

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/18/2026, 11:48:30 AM
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int inc = 0;
+        int dec = 0;
+
+        for(int i=0; i<operations.size(); i++){
+            if(operations[i] == "X++" || operations[i] == "++X"){
+                inc++;
+            }
+            else{
+                dec++;
+            }
+        }
+        return inc - dec;
+    }
+};
