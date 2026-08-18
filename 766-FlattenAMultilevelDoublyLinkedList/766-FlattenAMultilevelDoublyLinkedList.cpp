8165af92bc079018fcd2e680fba361752fd7a3e2
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:52:18 +0530

    Create 766-FlattenAMultilevelDoublyLinkedList

---
+++ 766-FlattenAMultilevelDoublyLinkedList/766-FlattenAMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,40 @@
+// Last updated: 8/18/2026, 11:52:19 AM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+
+public:
+    Node* flatten(Node* head) {
+        Node* temp = head;
+        Node* tempS = head;
+        stack <Node*> st;
+
+        while(temp != nullptr){
+            if(temp -> child != nullptr) {
+                if(temp->next != nullptr)st.push(temp->next);
+                temp -> next = temp->child;
+                temp -> child = nullptr;
+                temp -> next -> prev = temp;
+            }
+            tempS = temp;
+            temp = temp -> next;
+        }
+        while(!st.empty()){
+            temp = st.top();
+            st.pop();
+            tempS -> next = temp;
+            temp-> prev = tempS;
+            while(tempS->next != nullptr) tempS = tempS -> next; 
+        }
+        return head;
+    }
+};
