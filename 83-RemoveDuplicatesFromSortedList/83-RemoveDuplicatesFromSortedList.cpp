3b3d240cd07847db7d28d279e5ddb947c690ef36
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:54:40 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/18/2026, 11:54:42 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+
+        if(head == nullptr){
+            return head;
+        }
+
+        while(curr != nullptr && curr->next != nullptr){
+            if(curr->val == curr->next->val){
+                curr->next = curr->next->next;
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
