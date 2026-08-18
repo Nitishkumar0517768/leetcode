58a0feb42f7ec28063206197ff244172f52eaf2b
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-18 11:48:10 +0530

    Create 2254-CheckIfEveryRowAndColumnContainsAllNumbers

---
+++ 2254-CheckIfEveryRowAndColumnContainsAllNumbers/2254-CheckIfEveryRowAndColumnContainsAllNumbers.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/18/2026, 11:48:11 AM
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+
+        for(int i=0; i<matrix.size(); i++){
+        set<int> row, col;
+            for(int j=0; j<matrix.size(); j++){
+                row.insert(matrix[i][j]);
+                col.insert(matrix[j][i]);
+            }
+            if(row.size() != matrix.size() || col.size() != matrix.size()){
+                return false;
+            }
+        }
+        return true;
+    }
+};
