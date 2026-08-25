ba309eaf4a9963b4db1b3bf0e98f5d11a6f8e209
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-25 15:28:22 +0530

    File updated at 8/25/2026, 3:28:22 PM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,21 +1,35 @@
-// Last updated: 8/25/2026, 3:07:28 PM
+// Last updated: 8/25/2026, 3:28:22 PM
 1class Solution {
 2public:
 3    void rotate(vector<vector<int>>& matrix) {
-4        vector<vector<int>> ans;
+4        // vector<vector<int>> ans;
 5
-6        for(int i=0; i<matrix.size(); i++){
-7            vector<int> temp;
-8            for(int j=matrix.size()-1; j>=0; j--){
-9                temp.push_back(matrix[j][i]);
-10            }
-11            ans.push_back(temp);
-12        }
+6        // for(int i=0; i<matrix.size(); i++){
+7        //     vector<int> temp;
+8        //     for(int j=matrix.size()-1; j>=0; j--){
+9        //         temp.push_back(matrix[j][i]);
+10        //     }
+11        //     ans.push_back(temp);
+12        // }
 13
-14        for(int i=0; i<ans.size(); i++){
-15            for(int j=0; j<ans.size(); j++){
-16                matrix[i][j] = ans[i][j];
-17            }
-18        }
-19    }
-20};
+14        // for(int i=0; i<ans.size(); i++){
+15        //     for(int j=0; j<ans.size(); j++){
+16        //         matrix[i][j] = ans[i][j];
+17        //     }
+18        // }
+19
+20        
+21        // method-2
+22        int n = matrix.size();
+23
+24        for(int i=0; i<n; i++){
+25            for(int j=i+1; j<n; j++){
+26                swap(matrix[i][j], matrix[j][i]);
+27            }
+28        }
+29
+30        for(int i=0; i<n; i++){
+31            reverse(matrix[i].begin(), matrix[i].end());
+32        }
+33    }
+34};
