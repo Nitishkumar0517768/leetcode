b7f43a06eb14886ae44adaf1977933936a0a0d29
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-25 11:41:21 +0530

    Create 3816-DnaPatternRecognition

---
+++ 3816-DnaPatternRecognition/3816-DnaPatternRecognition.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/25/2026, 11:41:22 AM
+# Write your MySQL query statement below
+select sample_id, dna_sequence, species,
+(case when dna_sequence like "ATG%" then 1 else 0 end) as has_start,
+(case when dna_sequence like "%TAA" or dna_sequence like "%TAG" or dna_sequence like "%TGA" then 1 else 0 end) as has_stop,
+(case when dna_sequence like "%ATAT%" then 1 else 0 end) as has_atat,
+(case when dna_sequence like "%GGG%" then 1 else 0 end) as has_ggg
+from samples group by  sample_id;
