c3a580f4edbe96154ae12efe2d55a083dd13a053
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-25 11:52:37 +0530

    File updated at 8/25/2026, 11:52:38 AM

--- 3718-SmallestMissingMultipleofK/3718-SmallestMissingMultipleofK.cpp
+++ 3718-SmallestMissingMultipleofK/3718-SmallestMissingMultipleofK.cpp
@@ -1,23 +1,19 @@
-// Last updated: 8/25/2026, 11:49:50 AM
+// Last updated: 8/25/2026, 11:52:38 AM
 1class Solution {
 2public:
 3    int missingMultiple(vector<int>& nums, int k) {
-4        unordered_set<int> st;
+4        unordered_set<int> st(nums.begin(), nums.end());
 5
-6        for(int i=0; i<nums.size(); i++){
-7            st.insert(nums[i]);
-8        }
-9
-10        int n = 1;
-11
-12        for(int i=0; i<nums.size(); i++){
-13            int m = k * n;
-14
-15            if(!st.count(m)){
-16                return m;
-17            }
-18            n++;
-19        }
-20        return k*n;
-21    }
-22};
+6        int n = 1;
+7
+8        for(int i=0; i<nums.size(); i++){
+9            int m = k * n;
+10
+11            if(!st.count(m)){
+12                return m;
+13            }
+14            n++;
+15        }
+16        return k*n;
+17    }
+18};
