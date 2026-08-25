eeed8a4e47d3b5149c56347eb739beb1a684d978
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-08-25 11:43:49 +0530

    Create 1892-FindTotalTimeSpentByEachEmployee

---
+++ 1892-FindTotalTimeSpentByEachEmployee/1892-FindTotalTimeSpentByEachEmployee.sql
@@ -0,0 +1,3 @@
+-- Last updated: 8/25/2026, 11:43:49 AM
+# Write your MySQL query statement below
+select event_day as day, emp_id, sum(out_time - in_time) as total_time from employees group by event_day, emp_id;
