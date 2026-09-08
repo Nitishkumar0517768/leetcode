52bd20ee81652d8f473374891fe8808a474cc4fb
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-09-08 11:51:28 +0530

    File updated at 9/8/2026, 11:51:28 AM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
@@ -1,53 +1,53 @@
-// Last updated: 8/25/2026, 11:46:03 AM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-
-        if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
-            return head;
-        }
-
-        ListNode* curr = head;
-        ListNode* temp = nullptr;
-        ListNode* head1 = nullptr;
-
-        while(curr->next != nullptr && curr->next->next != nullptr){
-            
-            if(head1 == nullptr){
-                head1 = curr->next;
-                temp = head1;
-
-                curr->next = curr->next->next;
-                curr = curr->next;
-            }
-            else{
-                temp->next = curr->next;
-                temp = temp->next;
-
-                curr->next = curr->next->next;
-                curr = curr->next;
-            }
-        }
-
-        if(curr->next != nullptr){
-            temp->next = curr->next;
-            temp = temp->next;
-            curr->next = nullptr;
-        }
-
-        temp->next = nullptr;
-        curr->next = head1;
-
-        return head;
-    }
-};
+// Last updated: 9/8/2026, 11:51:28 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* oddEvenList(ListNode* head) {
+14
+15        if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
+16            return head;
+17        }
+18
+19        ListNode* curr = head;
+20        ListNode* temp = nullptr;
+21        ListNode* head1 = nullptr;
+22
+23        while(curr->next != nullptr && curr->next->next != nullptr){
+24            
+25            if(head1 == nullptr){
+26                head1 = curr->next;
+27                temp = head1;
+28
+29                curr->next = curr->next->next;
+30                curr = curr->next;
+31            }
+32            else{
+33                temp->next = curr->next;
+34                temp = temp->next;
+35
+36                curr->next = curr->next->next;
+37                curr = curr->next;
+38            }
+39        }
+40
+41        if(curr->next != nullptr){
+42            temp->next = curr->next;
+43            temp = temp->next;
+44            curr->next = nullptr;
+45        }
+46
+47        temp->next = nullptr;
+48        curr->next = head1;
+49
+50        return head;
+51    }
+52};
