f6db608e3a4ab83c64d8c9aac92c5b646cb52156
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-09-17 11:44:46 +0530

    File updated at 9/17/2026, 11:44:45 AM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/17/2026, 11:06:09 AM
+// Last updated: 9/17/2026, 11:44:45 AM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -11,25 +11,27 @@
 10 */
 11class Solution {
 12public:
-13    ListNode* deleteMiddle(ListNode* head) {
-14        if(head == nullptr){
-15            return head;
-16        }
-17
-18        ListNode* slow = head;
-19        ListNode* fast = head;
-20        ListNode* temp = new ListNode(0);
-21        ListNode* curr = temp; 
-22
-23        while(fast != nullptr && fast->next != nullptr){
-24            curr->next = slow;
-25            curr = curr->next;
-26            slow = slow->next;
-27            fast = fast->next->next;
-28        }
+13    ListNode* sortList(ListNode* head) {
+14        vector<int> nums;
+15
+16        ListNode* curr = head;
+17        ListNode* temp = new ListNode(0);
+18        ListNode* head2 = temp;
+19
+20        while(curr != nullptr){
+21            nums.push_back(curr->val);
+22            curr = curr->next;
+23        }
+24
+25        sort(nums.begin(), nums.end());
+26
+27        for(int i=0; i<nums.size(); i++){
+28            ListNode* list = new ListNode(nums[i]);
 29
-30        curr->next = slow->next;
-31
-32        return temp->next;
-33    }
-34};
+30            temp->next = list;
+31            temp = temp->next;
+32        }
+33
+34        return head2->next;
+35    }
+36};
