f36b20ba29b2a25530fffd747d0909c791b115c3
Nitish kumar <nitish.kumar.05cg@gmail.com>
2026-09-24 13:52:28 +0530

    Create 3550-SmallestIndexWithDigitSumEqualtoIndex

---
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/24/2026, 1:52:29 PM
+1class Solution {
+2public:
+3    int smallestIndex(vector<int>& nums) {
+4        for(int i=0; i<nums.size(); i++){
+5            int sumDigit = 0;
+6            int n = nums[i];
+7            while(n>0){
+8                sumDigit += n%10;
+9                n /= 10;
+10            }
+11            // cout << sumDigit << endl;
+12            if(sumDigit == i) return i;
+13        }
+14        return -1;
+15    }
+16};
